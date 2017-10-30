9e76c2269df94067ae42f5e540a6acaf7a5174ff
GitHubdeWill <hd209458inj@gmail.com>
2017-10-30 16:45:30 -0400

    first

---
+++ calendarPraser.py
@@ -0,0 +1,150 @@
+import os
+import sys
+import time
+import math
+from datetime import datetime, timedelta
+import icalendar
+from pytz import UTC
+from crontab import CronTab
+import requests
+import shutil
+
+# Print iterations progress
+def printProgressBar(iteration, total, prefix='>progress: ', suffix='complete',
+                     decimals=1, length=50, fill='#'):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    sys.stdout.write('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix))
+    # Print New Line on Complete
+    if iteration == total:
+        print ""
+
+def main():
+    '''take path of calendar file and '''
+    # Testing
+    initialTest()
+
+    # gcal = getCal(sys.argv[1])
+    # printEventList(gcal)
+
+    # comm = "date"
+    # scheduleEvent(gcal, comm)
+
+    return 0
+
+def updateCal(local_path, url):
+    '''
+        Get the latest calendar from url
+        Please specify the port number in URL
+    '''
+    r = requests.get(url, stream=True)
+    if r.status_code == 200:
+        with open(local_path, 'wb') as f:
+            r.raw.decode_content = True
+            shutil.copyfileobj(r.raw, f)
+
+def getCal(filename):
+    ''' return Calendar Object from .ics File'''
+    g = open(filename, 'rb')
+    gcal = icalendar.Calendar.from_ical(g.read())
+    return gcal
+
+def printComponentName(gcal):
+    ''' Find all components from Calendar'''
+    print '>component list:'
+    for component in gcal.walk():
+        print component.name
+
+def printEventList(gcal):
+    ''' Get all details of all scheduled VEVENTs'''
+    print '>event list'
+    for component in gcal.walk():
+        if component.name == "VEVENT":
+            print component.get('summary')
+
+def printEventDetail(gcal):
+    ''' Get all details of all scheduled VEVENTs'''
+    print '>event list'
+    for component in gcal.walk():
+        if component.name == "VEVENT":
+            print component.get('summary')
+            print component.get('dtstart').dt
+            print component.get('dtend').dt
+            print component.get('dtstamp').dt
+            if component.get('rrule') is not None:
+                print component.get('rrule').iteritems()
+
+def scheduleEvent(gcal, comm):
+    ''' Get all details of all scheduled VEVENTs'''
+    print '>event list'
+    for component in gcal.walk():
+        if component.name == "VEVENT":
+            summary = component.get('summary')
+            start_time = component.get('dtstart').dt
+            end_time = component.get('dtend').dt
+            time_delta = end_time - start_time
+
+            # Create Cron Job base on schedule
+            seconds = time_delta.seconds
+            seconds = seconds % 60
+            comm0 = comm
+            # + summary + " " + str(seconds)
+            createCronJob(comm0, start_time)
+
+            # print component.get('dtstamp').dt
+            # if component.get('rrule') is not None:
+            #     print component.get('rrule').iteritems()
+
+def createCronJob(comm, dt):
+    '''Create Cron Job with Console Command and datetime'''
+    print ""
+    cron = CronTab(user=True)
+    job = cron.new(command=comm, user='root')
+    job.setall(dt)
+    # job_std_out = job.run()
+    job.enable()
+    cron.write()
+    print comm + " scheduled at " + str(dt)
+
+
+def initialTest():
+    cal = icalendar.Calendar()
+    cal.add('prodid', '-//My calendar//umass.edu//')
+    cal.add('version', '2.0')
+
+    event = icalendar.Event()
+    event.add('summary', 'Python meeting about calendaring')
+    b = datetime.now() + timedelta(0,30)
+    event.add('dtstart', b)
+    event.add('dtend', b+ timedelta(0,10))
+    event.add('dtstamp', datetime(2017,4,4,0,10,0,tzinfo=UTC))
+    event['uid'] = '20170115T101010/ziweihe@umass.edu'
+    event.add('priority', 5)
+
+    cal.add_component(event)
+
+    f = open('temp.ics', 'wb')
+    f.write(cal.to_ical())
+    f.close()
+
+    # Testing
+    gcal = getCal('temp.ics')
+    printComponentName(gcal)
+    print ''
+    printEventDetail(gcal)
+
+    print "\n"
+
+    comm = "date > test.txt"
+    scheduleEvent(gcal, comm)
+
+    # gcal = getCal('test.ics')
+    # printComponentName(gcal)
+    # print ''
+    # printEventDetail(gcal)
+
+    os.remove('temp.ics')
+
+if __name__ == "__main__":
+    main()
